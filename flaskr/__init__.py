8cf0ad1d234dae61b14af64eb1b8ed68f71cebbd
silktreesolutions <joel.r.julian0408@gmail.com>
2019-07-22 18:58:24 +0530

    Registering blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import auth
 
 def create_app():
     app = Flask(__name__)
@@ -9,4 +10,6 @@ def create_app():
     def hello():
         return 'Hello World!'
 
+    app.register_blueprint(auth.bp)
+    app.add_url_rule('/', endpoint='index')
     return app
